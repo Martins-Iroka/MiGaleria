e60ec05dff4ced42e010d1a40dd70bacaaa5e048
Ikechukwu Iroka <martins.iroka@gmail.com>
2025-09-19 14:03:29 +0100

    Configure Gradle for JVM and update dependencies
    This commit introduces the following changes:
    
    - Configures Gradle to support JVM projects by adding the `jetbrains.kotlin.jvm` plugin.
    - Updates various dependencies in `libs.versions.toml`, including AGP, Android Tools, Compose, Ktor, MockK, and Timber.
    - Sets the Kotlin JPS plugin version to 1.9.0.
    - Sets the JVM target to 17 for Kotlin2JvmCompilerArguments and bytecodeTargetLevel.
    - Configures external storage and project JDK to 17 in `misc.xml`.
    - Adds build-logic and build-logic/convention modules to the Gradle settings.
    
    Signed-off-by: Ikechukwu Iroka <martins.iroka@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
