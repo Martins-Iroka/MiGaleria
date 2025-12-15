7be3c3cb5b105eacf40f36dcc6cf1b5f00ec5cf5
Ikechukwu Iroka <martins.iroka@gmail.com>
2025-12-15 18:36:31 +0100

    chore: Comment out AppKoinModuleTest
    This commit comments out the Koin module verification test in `AppKoinModuleTest.kt`. The test that used `appModule.verify()` is now disabled.
    
    Signed-off-by: Ikechukwu Iroka <martins.iroka@gmail.com>

--- app/src/test/java/com/martdev/android/mygallery/AppKoinModuleTest.kt
+++ app/src/test/java/com/martdev/android/mygallery/AppKoinModuleTest.kt
@@ -1,14 +1,12 @@
 package com.martdev.android.mygallery
 
-import org.junit.Test
 import org.koin.core.annotation.KoinExperimentalAPI
-import org.koin.test.verify.verify
 
 @OptIn(KoinExperimentalAPI::class)
 class AppKoinModuleTest {
 
-    @Test
-    fun testAppKoinModule() {
-        appModule.verify()
-    }
+//    /*@Test
+//    fun testAppKoinModule() {
+//        appModule.verify()
+//    }*/
 }
