740b7cabf93f5bc9818a00e7a07d1ab78b239f12
Ikechukwu Iroka <martins.iroka@gmail.com>
2026-02-12 17:37:21 +0100

    feat: Refactor DateConverter to use ZonedDateTime for parsing
    This commit updates the `DateConverter` utility to improve date string parsing
    
    ### Key Changes:
    
    - **Core & Common**:
        - **Date Conversion**: Updated `convertToUTCDateTime` to use `ZonedDateTime.parse` instead of `Instant.parse`. This ensures compatibility with input strings containing zone offsets while maintaining the same localized output format ("MMM dd, yyyy HH:mm").
    
    Signed-off-by: Ikechukwu Iroka <martins.iroka@gmail.com>

--- core/common/src/main/java/com/martdev/common/DateConverter.kt
+++ core/common/src/main/java/com/martdev/common/DateConverter.kt
@@ -1,13 +1,13 @@
 package com.martdev.common
 
-import java.time.Instant
 import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
 fun convertToUTCDateTime(utc: String): String {
-    val instant = Instant.parse(utc)
-    return DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm")
-        .withZone(ZoneId.systemDefault()).format(instant)
+     val dtf = DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm")
+        .withZone(ZoneId.systemDefault())
+    return ZonedDateTime.parse(utc).format(dtf)
 }
